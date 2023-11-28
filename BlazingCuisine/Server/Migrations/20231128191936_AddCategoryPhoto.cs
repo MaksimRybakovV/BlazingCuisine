57c302d3353fe4efbed860b79a0d5205157dabd6
MaksimRybakovV <mrybakov1406@gmail.com>
2023-11-29 01:56:06 +0500

    A carousel has been made to display recipe categories.

---
+++ BlazingCuisine/Server/Migrations/20231128191936_AddCategoryPhoto.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BlazingCuisine.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCategoryPhoto : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Photo",
+                table: "Categories",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Photo",
+                table: "Categories");
+        }
+    }
+}
